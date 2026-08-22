8b834e68630c2eb7e6c300b1a7b786ff1f41dd10
felges <felixrafael2702@gmail.com>
2026-08-22 11:22:08 -0400

    fix: clean scrapers init to only import shopify_scraper

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,18 +1,4 @@
-from scrapers.base_scraper import BaseScraper, Producto
-from scrapers.shopping_aggregator import ShoppingAggregatorScraper
-from scrapers.mercadolibre import MercadoLibreScraper
-from scrapers.ebay import EbayScraper
-from scrapers.aliexpress import AliExpressScraper
-from scrapers.amazon import AmazonScraper
-from scrapers.shein import SheinScraper
+# scrapers package
+from scrapers.shopify_scraper import ShopifyScraper
 
-__all__ = [
-    "BaseScraper",
-    "Producto",
-    "ShoppingAggregatorScraper",
-    "MercadoLibreScraper",
-    "EbayScraper",
-    "AliExpressScraper",
-    "AmazonScraper",
-    "SheinScraper",
-]
+__all__ = ["ShopifyScraper"]
